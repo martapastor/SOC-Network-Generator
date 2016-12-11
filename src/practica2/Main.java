571635d3e4cbaa3492505f30f2b15f5f3252a2c2
Marta Pastor Puente <marta.pastor@live.com>
2016-12-11 20:24:26 +0100

    Added GUI and implemented random network generator.

---
+++ src/practica2/Main.java
@@ -0,0 +1,13 @@
+package practica2;
+
+public class Main {
+	
+	public static void generateNetwork() {
+		new SwingView();
+	}
+	
+	public static void main(String[] args) {
+		
+		generateNetwork();
+	}
+}
